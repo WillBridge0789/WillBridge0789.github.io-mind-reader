763325fea4f034c9eb2ed94cfcf2c9e2fe2c27dd
WillBridge0789 <will.burbridge@student.awesomeinc.org>
2023-02-09 22:30:38 +0000

    Possible solution, but incomplete

--- js/main.js
+++ js/main.js
@@ -1,64 +1,71 @@
 //Variables:
-const resetButton;
-const currentView;
-const nextButton;
-let headerText;
-let paragraphText;
-
+//let resetButton = document.getElementById("resetButton");
+//let nextButton = document.getElementById("nextButton");
+let headerElement = document.getElementById('headerElement');
+let headerElement2 = document.getElementById('headerElement2');
+let headerElement3 = document.getElementById('headerElement3');
+let headerElement4 = document.getElementById('headerElement4');
+let headerElement5 = document.getElementById('headerElement5');
+let headerElement6 = document.getElementById('headerElement6');
+let paragraphTxt1 = document.getElementById('paragraphTxt1');
+let paragraphTxt2 = document.getElementById('pargraphTxt2');
+let paragraphTxt3 = document.getElementById('pargraphTxt2');
+let paragraphTxt4 = document.getElementById('pargraphTxt2');
+let paragraphTxt5 = document.getElementById('pargraphTxt2');
+let paragraphTxt6 = document.getElementById('pargraphTxt2');
+let paragraphTxt7 = document.getElementById('pargraphTxt2');
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '('];
 //Objects:
 let state = {
-    symbols: ['!', '@', '#', '$'. '%', '^', '&', '*', '('],
-    currentPage: 1,
-    pages = [
+    currentPage: 0,
+    pages:  [
         {
-          headerText: "I can read your mind",
-          paragraphText: "",
-          nextButtonText: ""
+            headerElement: "I can read your mind",
+            nextButton: "GO"
         },
         {
-          headerText: "Pick a number between 01-99",
-          paragraphText: "When you have your number, click next",
-          nextButtonText: "NEXT"
-        },
-        {
-            headerText: "Add both digits together to get a new number",
-            paragraphText: "Ex: 14 is 1 + 4 = 5" + " click next to proceed",
+            headerElement2: "Pick a number between 01-99",
+            paragraphTxt1: "When you have your number, click next",
             nextButtonText: "NEXT"
         },
         {
-            headerText: "Subtract your new number from your original number",
-            paragraphText: "Ex: 14 - 5 = 9" + " click next to proceed",
+            headerElement3: "Add both digits together to get a new number",
+            paragraphTxt2: "Ex: 14 is 1 + 4 = 5",
+            paragraphTxt3: " click next to proceed",
             nextButtonText: "NEXT"
         },
         {
-            headerText: "0-!, 1-@, 2-#, 3-$",
-            paragraphText: "Find your new number. Note the symbol thats by the number.",
+            headerElement4: "Subtract your new number from your original number",
+            paragraphTxt4: "Ex: 14 - 5 = 9",
+            parapraghTxt5: " click next to proceed",
+            nextButtonText: "NEXT"
+        },
+        {
+            headerElement5: "0-!, 1-@, 2-#, 3-$ ...",
+            paragraphTxt6: "Find your new number.", 
+            paragraphTxt7: "Note the symbol thats by the number.",
             nextButtonText: "REVEAL"
         },
         {
-            headerText: create99Symbols(),
+            headerElement6: create99Symbols(),
             paragraphText: "",
             nextButtonText: ""
         },
     ]
 }
 
+
 //Functions:
-function setPage() {
 
-}
-
-function reset() {
-
-}
+document.addEventListener("click", function reset() {
+    return state.pages[0];
+})
 
 function create99Symbols() {
-    let symbols = ['!', '@', '#', '$'. '%', '^', '&', '*', '('];
+    let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '('];
     let arr = [];
     for (let i = 0; i < 100; i++) {
         arr.push(i + " " + symbols[i % 9]);        
     }
+    return symbols;
 }
-btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
-});
